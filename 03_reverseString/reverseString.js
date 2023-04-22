ba3f68dad4ec9c341a998b7903cb085d809fd20f
mohammedalfa <mohammedrafid2006@yahoo.com>
2023-04-21 22:17:58 -0400

    completed up to 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let tmp = '';
+    for (let i = 0, n = string.length; i < n / 2; i++) {
+        tmp = stringArray[i];
+        stringArray[i] = stringArray[n - i];
+        stringArray[n - i] = tmp;
+    }
+    return stringArray.join("");
 };
 
 // Do not edit below this line
