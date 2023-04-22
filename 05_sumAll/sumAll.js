bd89616c311068559637e2cbb4de4de73519b0a5
mohammedalfa <mohammedrafid2006@yahoo.com>
2023-04-21 22:39:57 -0400

    complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+    if (startNumber < 0 || endNumber < 0) {
+        return 'ERROR';
+    }
+    if (typeof startNumber !== 'number' || typeof endNumber !== 'number') {
+        return 'ERROR';
+    }
+    if (startNumber > endNumber) {
+        [startNumber, endNumber] = [endNumber, startNumber];
+    }
+    let sum = endNumber;
 
+    for(let i = startNumber; i < endNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
