7bf19738be79066ad4c2f3e29a0400c2102cd7ef
mohammedalfa <mohammedrafid2006@yahoo.com>
2023-04-21 22:31:02 -0400

    completed 05

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+    if (number < 0) {
+        return 'ERROR';
+    }
+    let string = '';
+    for (let i = 0; i < number; i++) {
+        string = string.concat(text);
+    }
+    return string;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    for (let i = 0, n = array.length; i < n; i++) {
+        if (elements.includes(array[i])) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
