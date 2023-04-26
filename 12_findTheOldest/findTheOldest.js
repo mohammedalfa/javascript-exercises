012ca6bb1338c89fed152a2802519c03cd66f6a0
mohammedalfa <mohammedrafid2006@yahoo.com>
2023-04-25 22:59:09 -0400

    complete all exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    oldest = people.sort((a, b) => {
+        let aAge = ((a.yearOfDeath) ? a.yearOfDeath : currentYear) - a.yearOfBirth;
+        let bAge = ((b.yearOfDeath) ? b.yearOfDeath : currentYear) - b.yearOfBirth;
+        if (aAge > bAge) return 1
+        else return -1;
+    });
+    return oldest.pop();
 };
 
 // Do not edit below this line
