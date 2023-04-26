8ccc96c3d2dfaa00e05f2de67746c3e915997bff
mohammedalfa <mohammedrafid2006@yahoo.com>
2023-04-25 21:59:45 -0400

    complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((prod, num) => prod * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	return a <= 0 ? 1 : a * factorial(a - 1);
 };
 
 // Do not edit below this line
