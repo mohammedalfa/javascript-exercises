4f590a1b5b632cc32e003d3e5c5639dd85556d1a
mohammedalfa <mohammedrafid2006@yahoo.com>
2023-04-26 18:01:58 -0400

    complete all exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    num = Number(num);
+    let fib = [1, 1];
+    for (let i = 2; i < num; i++) {
+        fib.push(fib[i - 2] + fib[i - 1]);
+    }
+    return fib.pop()
 };
 
 // Do not edit below this line
